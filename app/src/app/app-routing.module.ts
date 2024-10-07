0bf643a82db3160f71890b4a51f61896e6c83fbc
Jorgenettodev <jorgenetto96@gmail.com>
2024-10-07 05:04:42 -0300

    Cria rotas "/" e "/adicionar-produtos"

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListagemProdutosComponent } from './components/views/produtos/listagem-produtos/listagem-produtos.component';
+import { AdicionarProdutosComponent } from './components/views/produtos/adicionar-produtos/adicionar-produtos.component';
 
 const routes: Routes = [
-
+  { path: '', component: ListagemProdutosComponent },
+  { path: 'adicionar-produto', component: AdicionarProdutosComponent }
 ];
 
 @NgModule({
