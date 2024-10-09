366bf08a3ad4063b740560deecd81b5c6f941d03
Jorgenettodev <jorgenetto96@gmail.com>
2024-10-09 05:58:10 -0300

    adds logic to navigate to the home page after adding a cake

--- app/src/app/components/views/produtos/adicionar-produtos/adicionar-produtos.component.ts
+++ app/src/app/components/views/produtos/adicionar-produtos/adicionar-produtos.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Produto } from '../../../../models/produto';
 import { Observable } from 'rxjs';
 import { ProdutoService } from '../../../../services/produto.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adicionar-produtos',
@@ -16,9 +17,9 @@ export class AdicionarProdutosComponent {
   public descricao!: string;
   public url_imagem!: string;
 
-  constructor(private http: HttpClient, private service: ProdutoService) {
-
-  }
+  constructor(private http: HttpClient,
+              private service: ProdutoService,
+              private router: Router) {}
 
   /* Cria um novo método que:
   *   - Cria um novo produto, pegando os dados lá do user form
@@ -32,6 +33,7 @@ export class AdicionarProdutosComponent {
     console.log(novoProduto);
     this.service.adicionarProduto(novoProduto).subscribe(response => {
       console.log(response);
+      this.router.navigate(['/']);
     });
   }
 
